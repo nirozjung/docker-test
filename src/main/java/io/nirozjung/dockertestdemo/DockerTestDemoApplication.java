ad3c41de6c8b41352fa7d8ba3a0d35a534490f74
nirozjung <zorinzung@gmail.com>
2020-04-08 22:04:33 +0200

    First commit

---
+++ src/main/java/io/nirozjung/dockertestdemo/DockerTestDemoApplication.java
@@ -0,0 +1,13 @@
+package io.nirozjung.dockertestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nirozjung/dockertestdemo/DockerTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nirozjung.dockertestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
